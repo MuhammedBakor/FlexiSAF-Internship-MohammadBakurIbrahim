92800f534d5d3a038c690b7541ed865b6d5fbe01
MohammadBakur <53197043+MuhammedBakor@users.noreply.github.com>
2025-09-14 21:54:47 +0300

    Delete wee1/backend directory

--- wee1/backend/src/main/java/com/moba/backend/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.moba.backend.repositories;
-
-import com.moba.backend.models.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserData, Integer> {
-
-}
